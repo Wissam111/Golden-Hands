ef9d3cf87e60de09ac4340a3700dbc2e703cfcbb
tarik husin <tarik.id.10@hotmail.com>
2022-11-13 13:09:19 +0200

    custom drawer

--- presentation/Navigation.js
+++ presentation/Navigation.js
@@ -3,27 +3,33 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "./screens/home/Home";
 import BookAppointmentScreen from "./screens/BookAppointmentScreen";
+import CustomDrawer from "./components/CustomDrawer";
 
 const HomeStack = createNativeStackNavigator();
 
 const HomeNavigation = () => {
-    return (
-        <HomeStack.Navigator screenOptions={{ headerShown: false }}>
-            <HomeStack.Screen name='HomeScreen' component={Home} />
-            <HomeStack.Screen name='BookAppointment' component={BookAppointmentScreen} />
-        </HomeStack.Navigator>
-    )
+  return (
+    <HomeStack.Navigator screenOptions={{ headerShown: false }}>
+      <HomeStack.Screen name='HomeScreen' component={Home} />
+      <HomeStack.Screen name='BookAppointment' component={BookAppointmentScreen} />
+    </HomeStack.Navigator>
+  )
 }
 
 
 const Drawer = createDrawerNavigator()
 
 const DrawerNavigation = () => {
-    return (
-        <Drawer.Navigator useLegacyImplementation screenOptions={{ headerShown: false }}>
-            <Drawer.Screen name="Home" component={HomeNavigation} />
-        </Drawer.Navigator>
-    )
+  return (
+    <Drawer.Navigator
+      drawerContentOptions={{
+        activeTintColor: '#D09900',
+        itemStyle: { borderRadius: 0, marginVertical: 0, borderBottomWidth: 0.5, borderBottomColor: '#D09900' },
+      }}
+      useLegacyImplementation screenOptions={{ headerShown: false }} drawerContent={props => <CustomDrawer {...props} />}>
+      <Drawer.Screen name="Home" component={HomeNavigation} />
+    </Drawer.Navigator>
+  )
 }
 
 
