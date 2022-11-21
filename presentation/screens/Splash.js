2585b733b3d049600e38c87908879be3e67241a8
tarik husin <tarik.id.10@hotmail.com>
2022-11-21 20:03:55 +0200

    splash

--- presentation/screens/Splash.js
+++ presentation/screens/Splash.js
@@ -5,11 +5,10 @@ import { globalStyles, primaryColor, white } from "../styles/global";
 import Animated, { FadeIn, FadeInRight } from 'react-native-reanimated';
 
 
-const AnimatedSComponent = Animated.createAnimatedComponent(View);
+const AnimatedComponent = Animated.createAnimatedComponent(View);
 
 
 const Splash = ({ navigation }) => {
-
     useEffect(() => {
         setTimeout(() => {
             navigation.reset({
@@ -21,13 +20,11 @@ const Splash = ({ navigation }) => {
 
     return (
         <View style={{ flex: 1, backgroundColor: primaryColor, justifyContent: 'center', alignItems: 'center' }}>
-
-            <AnimatedSComponent entering={FadeInRight}>
+            <AnimatedComponent entering={FadeInRight}>
                 <Image source={require('../../assets/imgs/barber-logo.png')} />
                 <Spacer space={4} />
                 <Text style={{ ...globalStyles.font, color: white }}>let's get a hair cut</Text>
-            </AnimatedSComponent>
-
+            </AnimatedComponent>
         </View>
     );
 }
